c8aa7f27475950266afdae480f78f8e3e5ae24fb
fogasmarci <fogas.marton@gmail.com>
2023-12-13 13:29:42 +0100

    update test application properties

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,4 +1,2 @@
-INSERT INTO news (ID, TITLE, CONTENT, PUBLISHDATE)
-VALUES ('1', 'News about tickets', 'Ipsum Lorum', '2023-12-11');
-INSERT INTO news (ID, TITLE, CONTENT, PUBLISHDATE)
-VALUES ('2', 'Test Title', 'Test Content', '2023-12-11');
+INSERT INTO news (TITLE, CONTENT, PUBLISHDATE) VALUES ('News about tickets', 'Ipsum Lorum', '2023-12-11');
+INSERT INTO news (TITLE, CONTENT, PUBLISHDATE) VALUES ('Test Title', 'Test Content', '2023-12-11');
