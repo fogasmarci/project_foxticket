84b0f9aba0d8e20c9b2579a4a56792a263e24549
Flora Kecskes <kecskes.floraa@gmail.com>
2023-12-14 09:36:01 +0100

    remove commandLineRunner and add integration test for article controller

--- src/main/java/com/greenfoxacademy/springwebapp/BasicSpringProjectApplication.java
+++ src/main/java/com/greenfoxacademy/springwebapp/BasicSpringProjectApplication.java
@@ -1,31 +1,13 @@
 package com.greenfoxacademy.springwebapp;
 
-import com.greenfoxacademy.springwebapp.repositories.ArticleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicSpringProjectApplication implements CommandLineRunner {
-  private ArticleRepository articleRepository;
-
-  @Autowired
-  public BasicSpringProjectApplication(ArticleRepository articleRepository) {
-    this.articleRepository = articleRepository;
-  }
+public class BasicSpringProjectApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(BasicSpringProjectApplication.class, args);
-  }
-
-  @Override
-  public void run(String... args) throws Exception {
-  //    Article article1 = new Article("barmi", "Ipsum Lorum");
-  //    Article article2 = new Article("Road block", "akarmi, anything, fdgjkfdkjfdkjfdkjfdkjfdkjfdkj");
-  //
-  //    articleRepository.save(article1);
-  //    articleRepository.save(article2);
   }
 }
 
--- src/test/java/com/greenfoxacademy/springwebapp/units/ArticleControllerTest.java
+++ src/test/java/com/greenfoxacademy/springwebapp/units/ArticleControllerTest.java
@@ -1,32 +1,35 @@
-package com.greenfoxacademy.springwebapp.integrations;
+package com.greenfoxacademy.springwebapp.units;
 
 import com.greenfoxacademy.springwebapp.dtos.ArticleListDTO;
 import com.greenfoxacademy.springwebapp.models.Article;
 import com.greenfoxacademy.springwebapp.services.ArticleService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.isA;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource(locations = "classpath:application-test.properties")
+@ActiveProfiles("test")
 public class ArticleControllerTest {
+
   @Autowired
   MockMvc mvc;
-  @Autowired
+  @MockBean
   ArticleService articleService;
 
   @Test
@@ -34,40 +37,52 @@ public class ArticleControllerTest {
     List<Article> articles = new ArrayList<>();
     Article article1 = new Article("News about tickets", "Ipsum Lorum");
     article1.setPublishDate(LocalDate.of(2023, 12, 11));
-    article1.setId(1L);
     Article article2 = new Article("Test Title", "Test Content");
     article2.setPublishDate(LocalDate.of(2023, 12, 11));
-    article2.setId(2L);
     articles.add(article1);
     articles.add(article2);
 
     ArticleListDTO articleListDTO = new ArticleListDTO();
     articleListDTO.setArticles(articles);
 
+    Mockito.when(articleService.listArticles(null)).thenReturn(articleListDTO);
+
     mvc.perform(get("/api/news"))
         .andExpect(status().is(200))
+        .andExpect(jsonPath("$.articles", isA(ArrayList.class)))
         .andExpect(jsonPath("$.articles").value(hasSize(2)))
         .andExpect(jsonPath("$.articles[0].title").value("News about tickets"));
-
-    assertThat(articleService.listArticles(null)).usingRecursiveComparison().isEqualTo(articleListDTO);
   }
 
   @Test
-  void listArticles_WithSearchParam_ListsSearchedArticlesOnly() throws Exception {
+  void listArticles_WithParam_ListsSearchedArticles() throws Exception {
     List<Article> articles = new ArrayList<>();
-    Article article1 = new Article("Test Title", "Test Content");
-    article1.setPublishDate(LocalDate.of(2023, 12, 11));
-    article1.setId(2L);
-    articles.add(article1);
+
+    Article article = new Article("test title", "Test Content");
+    article.setPublishDate(LocalDate.of(2023, 12, 11));
+    articles.add(article);
 
     ArticleListDTO articleListDTO = new ArticleListDTO();
     articleListDTO.setArticles(articles);
 
-    mvc.perform(get("/api/news").queryParam("search", "Test"))
-        .andExpect(status().is(200))
-        .andExpect(jsonPath("$.articles").value(hasSize(1)))
-        .andExpect(jsonPath("$.articles[0].title").value("Test Title"));
+    Mockito.when(articleService.listArticles("test title")).thenReturn(articleListDTO);
 
-    assertThat(articleService.listArticles("Test")).usingRecursiveComparison().isEqualTo(articleListDTO);
+    mvc.perform(get("/api/news").param("search", "test title"))
+        .andExpect(status().is(200))
+        .andExpect(jsonPath("$.articles", isA(ArrayList.class)))
+        .andExpect(jsonPath("$.articles").value(hasSize(1)))
+        .andExpect(jsonPath("$.articles[0].title").value("test title"));
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
