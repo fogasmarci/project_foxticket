79f3a76b02fd0f76fe8db5ced68639454286d64a
Flora Kecskes <kecskes.floraa@gmail.com>
2023-12-15 11:07:35 +0100

    fix style problem after resolving merge conflict

--- src/test/java/com/greenfoxacademy/springwebapp/BasicSpringProjectApplicationTests.java
+++ src/test/java/com/greenfoxacademy/springwebapp/BasicSpringProjectApplicationTests.java
@@ -3,7 +3,6 @@ package com.greenfoxacademy.springwebapp;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @Disabled
