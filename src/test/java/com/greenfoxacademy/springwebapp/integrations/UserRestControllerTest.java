f0b8e5b73483c44f42e93c479d5079c53bdb45e3
Flora Kecskes <kecskes.floraa@gmail.com>
2023-12-15 11:55:20 +0100

    change data test sql file and fix test files

--- src/test/java/com/greenfoxacademy/springwebapp/integrations/UserRestControllerTest.java
+++ src/test/java/com/greenfoxacademy/springwebapp/integrations/UserRestControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class UserRestControllerTest {
   @Autowired
   MockMvc mockMvc;
