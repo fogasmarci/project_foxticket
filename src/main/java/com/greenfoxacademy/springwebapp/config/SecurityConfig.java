6f365ac4ac1eefa1d169a9719c5dc8bc2ad6da20
Flora Kecskes <kecskes.floraa@gmail.com>
2024-01-10 12:23:30 +0100

    fix test database setup with new entities added

--- src/main/java/com/greenfoxacademy/springwebapp/config/SecurityConfig.java
+++ src/main/java/com/greenfoxacademy/springwebapp/config/SecurityConfig.java
@@ -5,21 +5,22 @@ import com.greenfoxacademy.springwebapp.services.JpaUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
 
   private final JpaUserDetailsService myUserDetailsService;
@@ -33,15 +34,40 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable).httpBasic(AbstractHttpConfigurer::disable);
+    http.csrf(AbstractHttpConfigurer::disable);
     http.headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
     http.authorizeHttpRequests(auth -> auth
-            .requestMatchers("/users").permitAll()
+            .requestMatchers("/api/admin").hasRole("ADMIN")
+            .requestMatchers("/register").permitAll()
             .requestMatchers("/api/users/**").permitAll()
+            .requestMatchers("/api/news").permitAll()
             .anyRequest().authenticated())
+        .exceptionHandling(exceptionHandling ->
+            exceptionHandling
+                .accessDeniedHandler(accessDeniedHandler())
+                .authenticationEntryPoint(authenticationEntryPoint())
+        )
         .userDetailsService(myUserDetailsService)
         .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
+  }
+
+  @Bean
+  public AccessDeniedHandler accessDeniedHandler() {
+    return (request, response, accessDeniedException) -> {
+      response.setStatus(HttpStatus.FORBIDDEN.value());
+      response.getWriter().write("Access Denied: " + accessDeniedException.getMessage());
+      response.getWriter().flush();
+    };
+  }
+
+  @Bean
+  public AuthenticationEntryPoint authenticationEntryPoint() {
+    return (request, response, authException) -> {
+      response.setStatus(HttpStatus.UNAUTHORIZED.value());
+      response.getWriter().write("Unauthorized: " + authException.getMessage());
+      response.getWriter().flush();
+    };
   }
 
   @Bean
@@ -50,7 +76,7 @@ public class SecurityConfig {
   }
 
   @Bean
-  PasswordEncoder passwordEncoder() {
+  public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
 }
