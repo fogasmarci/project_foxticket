94f8cc8f4a49649d9b83a28c816fea52ab940041
Andras Nagy <130433888+Nagyandrasadam@users.noreply.github.com>
2024-01-12 01:07:44 +0100

    remove unused imports

--- src/main/java/com/greenfoxacademy/springwebapp/controllers/ArticleController.java
+++ src/main/java/com/greenfoxacademy/springwebapp/controllers/ArticleController.java
@@ -8,7 +8,6 @@ import com.greenfoxacademy.springwebapp.services.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestController
 public class ArticleController {
--- src/test/java/com/greenfoxacademy/springwebapp/integrations/ArticleControllerTest.java
+++ src/test/java/com/greenfoxacademy/springwebapp/integrations/ArticleControllerTest.java
@@ -24,9 +24,7 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
