64695d6f21a561f3780636be2d2b98e4c3a82919
Flora Kecskes <kecskes.floraa@gmail.com>
2024-01-16 10:40:35 +0100

    add integration tests for add-multiple-products-to-cart

--- src/test/java/com/greenfoxacademy/springwebapp/integrations/CartControllerTest.java
+++ src/test/java/com/greenfoxacademy/springwebapp/integrations/CartControllerTest.java
@@ -100,6 +100,77 @@ public class CartControllerTest {
         .andExpect(jsonPath("$.cart").value(hasSize(3)));
   }
 
+  @Test
+  void addProductToCart_WithValidProductId_AndAmount_ReturnsCartContentCorrectly() throws Exception {
+    LoginUserDTO loginUserDTO = new LoginUserDTO("user@user.user", "12345678");
+    String jwt = login(loginUserDTO);
+    ProductIdDTO productIdDTO = new ProductIdDTO(2L, 3);
+
+    mvc.perform(post("/api/cart").header("Authorization", "Bearer " + jwt)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(productIdDTO)))
+        .andExpect(status().is(200))
+        .andExpect(jsonPath("$.cart").value(hasSize(3)))
+        .andExpectAll(jsonPath("$['cart'][0]['product_id']").value(productIdDTO.getProductId()),
+            jsonPath("$['cart'][0]['name']").value("teszt bérlet 1"),
+            jsonPath("$['cart'][0]['price']").value(4000))
+        .andExpectAll(jsonPath("$['cart'][2]['product_id']").value(productIdDTO.getProductId()),
+            jsonPath("$['cart'][0]['name']").value("teszt bérlet 1"),
+            jsonPath("$['cart'][0]['price']").value(4000));
+  }
+
+  @Test
+  void addProductToCart_WithInValidProductId_AndValidAmount_ReturnsCorrectErrorMessage() throws Exception {
+    LoginUserDTO loginUserDTO = new LoginUserDTO("user@user.user", "12345678");
+    String jwt = login(loginUserDTO);
+    ProductIdDTO productIdDTO = new ProductIdDTO(50L, 3);
+
+    mvc.perform(post("/api/cart").header("Authorization", "Bearer " + jwt)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(productIdDTO)))
+        .andExpect(status().is(400))
+        .andExpect(jsonPath("$.error").value("Product doesn't exist."));
+  }
+
+  @Test
+  void addProductToCart_WithMissingProductId_AndValidAmount_ReturnsCorrectErrorMessage() throws Exception {
+    LoginUserDTO loginUserDTO = new LoginUserDTO("user@user.user", "12345678");
+    String jwt = login(loginUserDTO);
+    ProductIdDTO productIdDTO = new ProductIdDTO(null, 3);
+
+    mvc.perform(post("/api/cart").header("Authorization", "Bearer " + jwt)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(productIdDTO)))
+        .andExpect(status().is(400))
+        .andExpect(jsonPath("$.error").value("Product ID is required."));
+  }
+
+  @Test
+  void addProductToCart_WithValidProductId_AndNegativeAmount_DoesNotAddAnythingToCart() throws Exception {
+    LoginUserDTO loginUserDTO = new LoginUserDTO("user@user.user", "12345678");
+    String jwt = login(loginUserDTO);
+    ProductIdDTO productIdDTO = new ProductIdDTO(2L, -3);
+
+    mvc.perform(post("/api/cart").header("Authorization", "Bearer " + jwt)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(productIdDTO)))
+        .andExpect(status().is(200))
+        .andExpect(jsonPath("$.cart").value(hasSize(0)));
+  }
+
+  @Test
+  void addProductToCart_WithValidProductId_AndZeroAmount_DoesNotAddAnythingToCart() throws Exception {
+    LoginUserDTO loginUserDTO = new LoginUserDTO("user@user.user", "12345678");
+    String jwt = login(loginUserDTO);
+    ProductIdDTO productIdDTO = new ProductIdDTO(2L, 0);
+
+    mvc.perform(post("/api/cart").header("Authorization", "Bearer " + jwt)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(productIdDTO)))
+        .andExpect(status().is(200))
+        .andExpect(jsonPath("$.cart").value(hasSize(0)));
+  }
+
   private String login(LoginUserDTO loginUserDTO) throws Exception {
     String responseContent = mvc.perform(post("/api/users/login")
             .contentType(MediaType.APPLICATION_JSON)
