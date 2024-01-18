3504a29fb4c80e24e97bc7871aa737a99a8c138f
Andras Nagy <130433888+Nagyandrasadam@users.noreply.github.com>
2024-01-18 16:32:28 +0100

    aet data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO PRODUCT_TYPES (NAME)
+VALUES ('jegy'),
+       ('bérlet');
+
+INSERT INTO PRODUCTS (NAME, PRICE, DURATION, DESCRIPTION, TYPE_ID)
+VALUES ('vonaljegy', 480, 90, 'teszt1', 1),
+       ('havi diák bérlet', 4000, 9000, 'teszt2', 2),
+       ('havi bérlet', 9500, 9000, 'teszt3', 2);
+
+INSERT INTO news (TITLE, CONTENT, PUBLISH_DATE)
+VALUES ('barmi', 'Ipsum Lorum', '2023-12-13');
+INSERT INTO news (TITLE, CONTENT, PUBLISH_DATE)
+VALUES ('Road block', 'akarmi, anything, fdgjkfdkjfdkjfdkjfdkjfdkjfdkj', '2023-12-13');
+
+INSERT INTO `USERS` (`NAME`, `EMAIL`, `PASSWORD`, `ROLES`, `IS_ADMIN`, `IS_VERIFIED`)
+VALUES ('Admin', 'admin@admin.com', '$2a$10$x44csP50u/GqqeNtLW/44OLrzGq0taFv7nIb86aUw2gvrEJqiR8By',
+        'ROLE_USER,ROLE_ADMIN', TRUE, TRUE);
