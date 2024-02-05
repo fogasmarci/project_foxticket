afaffc436d3fd1177105210e3177e80f63d834c9
Flora Kecskes <kecskes.floraa@gmail.com>
2024-02-05 13:10:51 +0100

    undo last changes

--- src/test/java/com/greenfoxacademy/springwebapp/integrations/ArticleServiceTestWithH2.java
+++ src/test/java/com/greenfoxacademy/springwebapp/integrations/ArticleServiceTestWithH2.java
@@ -6,6 +6,7 @@ import com.greenfoxacademy.springwebapp.services.ArticleService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
