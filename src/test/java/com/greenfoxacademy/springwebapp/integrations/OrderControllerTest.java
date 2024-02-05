1a0b6144c10027e386d0305eb371552d614f0a2d
Andras Nagy <130433888+Nagyandrasadam@users.noreply.github.com>
2024-02-05 12:35:36 +0100

    resolve conflicts

--- src/test/java/com/greenfoxacademy/springwebapp/integrations/OrderControllerTest.java
+++ src/test/java/com/greenfoxacademy/springwebapp/integrations/OrderControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -28,7 +27,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @TestPropertySource(locations = "classpath:application-test.properties")
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class OrderControllerTest {
   @Autowired
   MockMvc mvc;
