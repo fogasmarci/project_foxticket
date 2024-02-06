be609e70518e975eaeb1d9c6b16dbb0dc9651546
Andras Nagy <130433888+Nagyandrasadam@users.noreply.github.com>
2024-02-06 10:24:53 +0100

    refactor validation in UserService

--- src/main/java/com/greenfoxacademy/springwebapp/dtos/RegistrationResponseDTO.java
+++ src/main/java/com/greenfoxacademy/springwebapp/dtos/RegistrationResponseDTO.java
@@ -1,4 +1,7 @@
 package com.greenfoxacademy.springwebapp.dtos;
 
 public record RegistrationResponseDTO(long id, String email, boolean isAdmin, String message) {
+  public RegistrationResponseDTO(long id, String email, boolean isAdmin) {
+    this(id, email, isAdmin, "Verification e-mail sent.");
+  }
 }
